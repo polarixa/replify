d6ad71806449fff9c311c1709bde5eb39ec68ae8
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2026-08-19 20:04:48 +0700

    :recycle: refactor: eliminate dead code and unused import statements

--- docs.go
+++ docs.go
@@ -16,15 +16,18 @@ import (
 //
 // The generated summary document can be used for logging, reporting, or any other purpose where a concise overview of the wrapper's state is needed.
 func (w *wrapper) SummaryDoc() *strchain.StringWeaver {
+	w.autoAdjust()
 	sw := strchain.New()
 	sw.Append("##").Space()
 	sw.Append("Summary").NewLines(2)
 	sw.Pipe().Space().Append("Field").Space().Pipe().Space().Append("Value").Space().Pipe().NewLine()
 	sw.Pipe().Dashes(3).Pipe().Dashes(3).Pipe().NewLine()
 
-	if strutil.IsNotEmpty(w.Message()) {
-		sw.Pipe().Space().Append("Message").Space().Pipe().Space().Underscore().Append(escapeMarkdownPipe(w.Message())).Underscore().Space().Pipe().NewLine()
-	}
+	sw.Pipe().Space().Append("Message").Space().Pipe().Space().
+		Underscore().
+		Append(strutil.DefaultIfEmpty(escapeMarkdownPipe(w.Message()), "<empty>")).
+		Underscore().Space().Pipe().NewLine()
+
 	if w.IsStatusCodePresent() {
 		sw.Pipe().Space().Append("Status Code").Space().Pipe().Space().AppendInt(w.StatusCode()).Space().Pipe().NewLine()
 		sw.Pipe().Space().Append("HTTP Status").Space().Pipe().Space().Append(escapeMarkdownPipe(w.StatusText())).Space().Pipe().NewLine()
@@ -43,18 +46,19 @@ func (w *wrapper) SummaryDoc() *strchain.StringWeaver {
 // The generated debug document can be used for logging, troubleshooting, or any other purpose where detailed debugging information is needed.
 func (w *wrapper) DebugDoc() *strchain.StringWeaver {
 	sw := strchain.New()
+	if !w.IsDebuggingPresent() {
+		return sw
+	}
 	sw.Append("##").Space()
 	sw.Append("Debug Information").NewLines(2)
 	sw.Pipe().Space().Append("Key").Space().Pipe().Space().Append("Value").Space().Pipe().NewLine()
 	sw.Pipe().Dashes(3).Pipe().Dashes(3).Pipe().NewLine()
 
-	if w.IsDebuggingPresent() {
-		for k, v := range w.Debugging() {
-			if k == "error_stack_trace" { // Skip the error stack trace in the debug document
-				continue
-			}
-			sw.Pipe().Space().Append(escapeMarkdownPipe(k)).Space().Pipe().Space().Append(escapeMarkdownPipe(conv.StringOrEmpty(v))).Space().Pipe().NewLine()
+	for k, v := range w.Debugging() {
+		if k == "error_stack_trace" { // Skip the error stack trace in the debug document
+			continue
 		}
+		sw.Pipe().Space().Append(escapeMarkdownPipe(k)).Space().Pipe().Space().Append(escapeMarkdownPipe(conv.StringOrEmpty(v))).Space().Pipe().NewLine()
 	}
 	return sw
 }
@@ -71,6 +75,7 @@ func (w *wrapper) ErrorStackTraceDoc() *strchain.StringWeaver {
 	if !w.IsError() {
 		return sw
 	}
+	w.autoAdjust()
 	w.InjectStackTrace() // Ensure the stack trace is injected before generating the document
 
 	sw.Append("##").Space()
@@ -115,17 +120,18 @@ func (w *wrapper) SafeErrorStackTraceDoc() *strchain.StringWeaver {
 // The generated header document can be used for logging, troubleshooting, or any other purpose where detailed header information is needed.
 func (w *wrapper) HeaderDoc() *strchain.StringWeaver {
 	sw := strchain.New()
+	if !w.IsHeaderPresent() {
+		return sw
+	}
 	sw.Append("##").Space()
 	sw.Append("Headers").NewLines(2)
 	sw.Pipe().Space().Append("Field").Space().Pipe().Space().Append("Value").Space().Pipe().NewLine()
 	sw.Pipe().Dashes(3).Pipe().Dashes(3).Pipe().NewLine()
 
-	if w.IsHeaderPresent() {
-		h := w.Header()
-		sw.Pipe().Space().Append("Code").Space().Pipe().Space().AppendInt(h.Code()).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Text").Space().Pipe().Space().Append(escapeMarkdownPipe(h.Text())).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Type").Space().Pipe().Space().Append(escapeMarkdownPipe(h.Type())).Space().Pipe().NewLine()
-	}
+	h := w.Header()
+	sw.Pipe().Space().Append("Code").Space().Pipe().Space().AppendInt(h.Code()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Text").Space().Pipe().Space().Append(escapeMarkdownPipe(h.Text())).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Type").Space().Pipe().Space().Append(escapeMarkdownPipe(h.Type())).Space().Pipe().NewLine()
 	return sw
 }
 
@@ -139,18 +145,46 @@ func (w *wrapper) HeaderDoc() *strchain.StringWeaver {
 // The generated pagination document can be used for logging, troubleshooting, or any other purpose where detailed pagination information is needed.
 func (w *wrapper) PagingDoc() *strchain.StringWeaver {
 	sw := strchain.New()
+	if !w.IsPagingPresent() {
+		return sw
+	}
 	sw.Append("##").Space()
 	sw.Append("Pagination").NewLines(2)
 	sw.Pipe().Space().Append("Field").Space().Pipe().Space().Append("Value").Space().Pipe().NewLine()
 	sw.Pipe().Dashes(3).Pipe().Dashes(3).Pipe().NewLine()
 
-	if w.IsPagingPresent() {
-		p := w.Pagination()
-		sw.Pipe().Space().Append("Page").Space().Pipe().Space().AppendInt(p.Page()).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Per Page").Space().Pipe().Space().AppendInt(p.PerPage()).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Total Items").Space().Pipe().Space().AppendInt(p.TotalItems()).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Total Pages").Space().Pipe().Space().AppendInt(p.TotalPages()).Space().Pipe().NewLine()
-		sw.Pipe().Space().Append("Is Last").Space().Pipe().Space().AppendBool(p.IsLast()).Space().Pipe().NewLine()
+	p := w.Pagination()
+	sw.Pipe().Space().Append("Page").Space().Pipe().Space().AppendInt(p.Page()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Per Page").Space().Pipe().Space().AppendInt(p.PerPage()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Total Items").Space().Pipe().Space().AppendInt(p.TotalItems()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Total Pages").Space().Pipe().Space().AppendInt(p.TotalPages()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Is Last").Space().Pipe().Space().AppendBool(p.IsLast()).Space().Pipe().NewLine()
+	return sw
+}
+
+// CursorDoc generates a document for the cursor information present in the wrapper instance, providing a detailed view of the cursor details. It returns a StringWeaver instance that can be used to build and format the cursor content.
+//
+// The cursor document includes the following information:
+//   - A header indicating that it is cursor information.
+//   - A table with two columns: "Field" and "Value".
+//   - If cursor information is present, each cursor field and its corresponding value will be included in the table.
+//
+// The generated cursor document can be used for logging, troubleshooting, or any other purpose where detailed cursor information is needed.
+func (w *wrapper) CursorDoc() *strchain.StringWeaver {
+	sw := strchain.New()
+	if !w.IsCursorPresent() {
+		return sw
 	}
+	sw.Append("##").Space()
+	sw.Append("Cursor").NewLines(2)
+	sw.Pipe().Space().Append("Field").Space().Pipe().Space().Append("Value").Space().Pipe().NewLine()
+	sw.Pipe().Dashes(3).Pipe().Dashes(3).Pipe().NewLine()
+
+	c := w.Cursor()
+	sw.Pipe().Space().Append("Next").Space().Pipe().Space().Append(escapeMarkdownPipe(c.Next())).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Previous").Space().Pipe().Space().Append(escapeMarkdownPipe(c.Previous())).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Has Next").Space().Pipe().Space().AppendBool(c.HasNext()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Has Previous").Space().Pipe().Space().AppendBool(c.HasPrevious()).Space().Pipe().NewLine()
+	sw.Pipe().Space().Append("Limit").Space().Pipe().Space().AppendInt(c.Limit()).Space().Pipe().NewLine()
 	return sw
 }
