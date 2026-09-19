47c67c2c3c2bcfaf7d7b00bc26d1bbe3f2ff923f
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2026-09-19 23:10:12 +0700

    :art: style: enforce consistent formatting across entire codebase

--- stream.go
+++ stream.go
@@ -16,6 +16,42 @@ import (
 	"github.com/polarixa/replify/pkg/strutil"
 )
 
+// Get returns buffer from pool
+//
+// If pool is empty, it creates a new buffer
+// of predefined size
+//
+// Returns:
+//   - A byte slice buffer
+func (bp *BufferPool) Get() []byte {
+	select {
+	case buf := <-bp.buffers:
+		return buf
+	default:
+		return make([]byte, bp.size)
+	}
+}
+
+// Put returns buffer to pool
+//
+// # If pool is full, the buffer is discarded
+//
+// Parameters:
+//   - buf: A byte slice buffer to be returned to the pool
+//
+// Returns:
+//   - None
+func (bp *BufferPool) Put(buf []byte) {
+	if buf == nil {
+		return
+	}
+	select {
+	case bp.buffers <- buf:
+	default:
+		// Pool is full, discard
+	}
+}
+
 // Start initiates streaming operation and returns *wrapper for consistency with replify API.
 //
 // This function is the primary entry point for streaming operations. It validates prerequisites (streaming wrapper
